21e34c18e94840474de9d6928ec414a5735cdcf2
mkqcc <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-08-25 21:39:45 -0400

    Finished creating seeds for orders and lemons

--- server/main.js
+++ server/main.js
@@ -3,14 +3,14 @@ const server = require('./index')
 const database = require('./db/database')
 
 const init = async () => {
-	try {
-		await database.sync()
-		server.listen(PORT, () => {
-			console.log(`Listening on ${PORT}`)
-		})
-	} catch (error) {
-		console.log(`Error occured while setting up server: ${error}`)
-	}
+    try {
+        await database.sync()
+        server.listen(PORT, () => {
+            console.log(`Listening on ${PORT}`)
+        })
+    } catch (error) {
+        console.log(`Error occured while setting up server: ${error}`)
+    }
 }
 
 init()
