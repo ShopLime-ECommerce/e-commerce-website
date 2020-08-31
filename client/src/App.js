b42e05afa1d4519972d8f024df84790ed18e12a4
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-08-31 10:53:04 -0400

    add front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Items from './components/Items'
+import UserInfo from './components/User'
 
-export default class App extends React.Component {
+export default class App extends React.Component { 
     render() {
         return (
             <div>
                 <NavBar />
             </div>
+
         )
     }
+
+    render() {
+        return (
+            <div>
+                <Items/>
+            </div>
+
+        )
+    }
+   
+    render() {
+        return (
+            <div>
+                <UserInfo/>
+            </div>
+
+        )
+    }
+
 }
+
---
+++ client/src/components/Items.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function items() {
+    return (
+        
+            <div className="listItems">
+            
+                  
+            
+           </div>
+        
+    )
+}
